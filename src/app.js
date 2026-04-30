6d0d0bb2243703ccc647acc39b116e568b1d5f38
Pushpendra <pushpendrapatel8055@gmail.com>
2026-04-30 22:47:56 +0530

    feat: initialize Express server in app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 // ─── CORS ────────────────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://google-review-qr-generator-fe.vercel.app"],
+    origin: ["http://localhost:5173", "https://google-review-qr-generator-fe-nine.vercel.app"],
     credentials: true,
   })
 );
